2a19e26e9fdcdb23a496c6d5f4d61df5ba32962c
kai <kai@hiraki.nl>
2026-05-30 14:18:14 +0200

    commit

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
    - Scryfall-zoekopdrachten: altijd via het netwerk
    ======================================================= */
 
-const CACHE_NAAM = "magic-meeting-v1";
+const CACHE_NAAM = "magic-meeting-v2";
 
 /* alle eigen bestanden die de app nodig heeft om te werken */
 const APP_SHELL = [
